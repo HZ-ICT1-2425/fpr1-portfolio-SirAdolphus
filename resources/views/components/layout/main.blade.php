1a5a0b487e2e6bb8cf32cf79f8fc4afc0613132d
SIrAdolphus <76489090+SIrAdolphus@users.noreply.github.com>
2025-02-14 12:46:20 +0100

    this is the weekly basically just need to do the routing anf blog button to add them but im to bored right now

---
+++ resources/views/components/layout/main.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <link rel="icon" href="fotos/website.icon.jpg" type="image">
+</head>
+
+
+<body>
+<!------Navigation------>
+
+<nav>
+    <ul class="nav">
+        <li class="nav">
+
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a target="_blank" href="https://github.com/HZ-HBO-ICT">Github HBO-ICT</a>
+                <a target="_blank" href="https://shorturl.at/5Yn47">PCO Notion</a>
+                <a target="_blank" href="https://hz.osiris-student.nl/voortgang">Progress MyHZ</a>
+                <a target="_blank" href="https://oer.hz.nl/6978cdea-fb31-430b-9bf9-63206aa07754">Regulations HBO-ICT</a>
+            </div>
+            <span style="font-size:30px;cursor:pointer;color:#5ba33e;" onclick="openNav()">&#9776; </span>
+
+            <script>
+                function openNav() {
+                    document.getElementById("mySidenav").style.width = "250px";
+                    document.getElementById("main").style.marginLeft = "250px";
+                }
+
+                function closeNav() {
+                    document.getElementById("mySidenav").style.width = "0";
+                    document.getElementById("main").style.marginLeft = "0";
+                }
+            </script>
+
+        </li>
+        <li class="nav"><a href="index.html">Home</a></li>
+        <li class="nav"><a href="profile.html">Profile</a></li>
+        <li class="nav"><a class="active" href="blog.html">Blog</a></li>
+        <li class="nav"><a href="dashboard.html">Dashboard</a></li>
+        <li class="nav"><a href="faq.html">FAQ</a></li>
+        <a href="https://hz.nl/"><img style="float: right;" id="groenLogoMagEigenlijkNiet"
+                                      src="/fotos/hzlogo-Photoroom.png" alt="hzLogo" class="illegaalgroenlogo"></a>
+        <li style="float: right;" class="nav"><button class="nav" onclick="changePhoto()">Hugo Button!</button></li>
+    </ul>
+
+    <script src="js/logoknop.js"></script>
+</nav>
+
+{{ $slot }}
+
+<section class="footer">
+    <a target="_blank" href="https://www.instagram.com/gijs_borghouts/?next=%2F"><img
+            src="/fotos/InstagramUpdated-Photoroom.png" alt="instagram" class="footerlogo"> </a>
+    <a target="_blank" href="https://github.com/SIrAdolphus"><img src="/fotos/gitUpdated-Photoroom.png" alt="github"
+                                                                  class="footerlogo"></a>
+    <a href="mailto:borg0039@hz.nl"><img src="/fotos/mailUpdated2-Photoroom.png" alt="Mail" class="footerlogo"></a>
+</section>
+</body>
+
+</html>
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,89 @@
+<x-layout.main>
+
+<h1 class="header">Frequently Asked Questions!</h1>
+<section class="blog">
+    <article class="article">
+        <p class="articledate">for working in groups!</p>
+        <h3 class="articletitle">How can you book a Project space?</h3>
+        <details>
+            <summary class="readmore">read more</summary>
+            <p>
+                <br> Step 1! - Go to your <a
+                    href="https://fs.myhz.nl/adfs/oauth2/authorize/?resource=urn%3AWebservice-hz_portal&state=349da54c8e316abdc7b72f41ae600cab&scope=openid&response_type=code&approval_prompt=auto&redirect_uri=https%3A%2F%2Fportal.hz.nl%2Fauth%2Fadfs%2Fcallback&client_id=b25d3143-ffab-46db-96bd-fff258b69da3&client-request-id=8ec2cecf-196e-412e-e300-008001040093&pullStatus=0">HZ
+                    Portal!</a>
+                <br> Step 2! - Go to the self service
+                <br> Step 3! - Click on reserve
+                <br> Step 4! - Choose the correct category and search for the "recource" you want to reserve.
+                <br> Step 5! - Select the day you're reserving this recource.
+                <br> Step 6! - By making a box over the time table indicate what time you will be reserving.
+                <br> Step 7! - Click 'Next'
+                <br> Step 8! - Read the conditions for borrowing, Check the reservation and click next.
+                <br> Step 9! - Fill in the required information and click confirm reservation.
+            </p>
+        </details>
+    </article>
+</section>
+<section class="blog">
+    <article class="article">
+        <p class="articledate">for all your physical papers!</p>
+        <h3 class="articletitle">How can you print a document from your laptop at HZ?</h3>
+        <details>
+            <summary class="readmore">read more</summary>
+            <p>
+                <br> Step 1! - Go to <a href="https://Mypaynet.nl/login">https://Mypaynet.nl/</a> or scan the qr by the
+                printer.
+                <br> Step 2! - Click on 'web print'
+                <br> Step 3! - upload your files and pick them up at the printer.
+            </p>
+        </details>
+    </article>
+</section>
+<section class="blog">
+    <article class="article">
+        <p class="articledate">for if you take the car!</p>
+        <h3 class="articletitle">What are the instructions for parking your car at HZ Middelburg?</h3>
+        <details>
+            <summary class="readmore">read more</summary>
+            <p>
+                <br>Step 1! - Park your car in the parking garage named 'Parkeergarage Kousteensedijk'.
+                <br>Step 2! - Walk towards the HZ building and do the things you cam to do!
+                <br>Step 3! - Walk in to the JRCZ and ask for a exit ticket at the counter. (you may have to show your school
+                pass)
+                <br>Step 4! - Walk towards your car and drive away!
+            </p>
+        </details>
+    </article>
+</section>
+<section class="blog">
+    <article class="article">
+        <p class="articledate">physical to digital! who doesn't like digital!</p>
+        <h3 class="articletitle">How can you scan a document and send it to your laptop at HZ?</h3>
+        <details>
+            <summary class="readmore">read more</summary>
+            <p>
+                <br> Step 1! - Scan your card at one of the various printers within HZ.
+                <br> Step 2! - Open the printer and put the paper you want to scan on the glassplate by the corner.
+                <br> Step 3! - Close the printer and select scan.
+                <br> step 4! - Follow the steps seen on the printer and press start.
+                <br> Step 5! - Don't forget to log out!
+            </p>
+        </details>
+    </article>
+</section>
+<section class="blog">
+    <article class="article">
+        <p class="articledate">for all your HZ merch! (and activities!)</p>
+        <h3 class="articletitle">How can I buy/sign-up something through th HZ Webshop?</h3>
+        <details>
+            <summary class="readmore">read more</summary>
+            <p>
+                <br> Step 1! - Go towards the HZ Webshop!
+                <br> Step 2! - Add the product/activities you want to your basket!
+                <br> Step 3! - Double check in your cart if you have everyting and the correct things.
+                <br> Step 4! - Order your products!
+            </p>
+        </details>
+    </article>
+</section>
+
+</x-layout.main>
